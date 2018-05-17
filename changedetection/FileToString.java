d1f641df7667a016fdcd747c651e4eea3d247047
Ellen (Yufei) Chen <chenellen007@gmail.com>
2018-05-16 19:14:32 -0700

    Merge

--- changedetection/FileToString.java
+++ changedetection/FileToString.java
@@ -8,13 +8,21 @@ public class FileToString{
      * @param filePath : String
      * @return String/null
      */
+<<<<<<< HEAD
     public static String convertToString(String filePath) {
+=======
+    public static String convertToString(String filePath){
+>>>>>>> Merge
         File file = new File(filePath);
         StringBuilder s = new StringBuilder();
         try {
             BufferedReader in = new BufferedReader(new FileReader(file));
             String buf = in.readLine();
             while (buf != null){
+<<<<<<< HEAD
+=======
+                System.out.println(buf);
+>>>>>>> Merge
                 if (!(buf.trim().equals(""))){
                     s.append(buf.trim());
                     s.append(" ");
@@ -22,15 +30,27 @@ public class FileToString{
                 buf = in.readLine();
             }
             return s.toString();
+<<<<<<< HEAD
         } catch(IOException iox) {
+=======
+        }catch(IOException iox) {
+>>>>>>> Merge
             iox.printStackTrace();
         }
         return null;
     }
 
+<<<<<<< HEAD
     public static void main(String[] args) {
+=======
+    public static void main(String[] args){
+>>>>>>> Merge
         String filePath = "/Users/Reece/Ellen's stuff/Projects/AuGit/changedetection/FunctionDescription.java";
         String res = convertToString(filePath);
         System.out.println(res);
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> Merge
