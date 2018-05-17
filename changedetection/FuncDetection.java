2784e2e4b98c4bafc463e754072b8a9a58ff6546
Reece Martin <capt.rm.fae@gmail.com>
2018-05-16 20:53:16 -0700

    Cleanup
    Added spaces and removed unnecessary padding

--- changedetection/FuncDetection.java
+++ changedetection/FuncDetection.java
@@ -16,18 +16,14 @@ public class FuncDetection {
     }
 
     //if str1 is null indexOfDifference will return 0 and str2 will be returned
-    if (str2 == null){
+    if (str2 == null) {
       return str1;
     }
-
     int changeIndex = indexOfDifference(str1, str2);
-
-    if (changeIndex == -1){
+    if (changeIndex == -1) {
       return "";
     }
-
     return str2.substring(changeIndex);
-
   }
 
   /**
@@ -38,17 +34,16 @@ public class FuncDetection {
    * @param str2 the second string to be compared with the first
    * @return String
    */
-  public static int indexOfDifference(String str1, String str2){
-    if (str1.equals(str2)){
+  public static int indexOfDifference(String str1, String str2) {
+    if (str1.equals(str2)) {
       return -1;
     }
-    if (str1 == null || str2 == null){
+    if (str1 == null || str2 == null) {
       return 0;
     }
-
     int i;
-    for (i = 0; i < str1.length() && i < str2.length(); i++){
-      if (str1.charAt(i) != str2.charAt(i)){
+    for (i = 0; i < str1.length() && i < str2.length(); i++) {
+      if (str1.charAt(i) != str2.charAt(i)) {
         return i;
       }
     }
@@ -62,7 +57,6 @@ public class FuncDetection {
      */
   public static void detectFunctions(String origin, String current) {
     String diff = difference(origin, current);
-      
   }
 
   public static String getMessage(ArrayList<FunctionDescription> functionDescriptions) {
