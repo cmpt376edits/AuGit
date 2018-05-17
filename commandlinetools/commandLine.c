203edabb8161dbf6cd188d6d07e00eb68c9203e7
Reece Martin <capt.rm.fae@gmail.com>
2018-05-17 07:56:03 -0700

    Update commandLine.c

--- commandlinetools/commandLine.c
+++ commandlinetools/commandLine.c
@@ -1,3 +1,9 @@
+
+
 int main (int argc, char* argv){
   //code
+  //roughly what we want to do is detect when a git commit is made then grab that input, 
+  // once we have that we look at the currently added files and do a generate message for
+  // them once we have that message we send git commit -m "<message>" to stdin and add an additional 
+  // that says AuGit wrote a commit
 }
