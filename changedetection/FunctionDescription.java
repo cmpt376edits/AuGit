be36f0246cac82459614cd0ce883e769c17cb73a
Reece Martin <capt.rm.fae@gmail.com>
2018-05-26 13:18:07 -0700

    Adding access modifiers

--- changedetection/FunctionDescription.java
+++ changedetection/FunctionDescription.java
@@ -33,17 +33,23 @@ public class FunctionDescription {
   public void setRetType(String retType) {
     this.retType = retType;
   }
+  
+  // Getter
+  
+  // Setter
 
   private String retType;
   private String functionName;
   private int numArgs;
   private ArrayList<String> args;
+  // Access Modifier
   
   // May need to expand definition to include access rights etc.
-  public FunctionDescription(String functionName, int numArgs, ArrayList<String> args, String retType) {
+  public FunctionDescription(String functionName, int numArgs, ArrayList<String> args, String retType, /* Added access modifier*/) {
     this.functionName = functionName;
     this.numArgs = numArgs;
     this.args = args;
     this.retType = retType;
+    // Access Modifier
   }
 }
