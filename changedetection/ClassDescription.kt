43229a7d3f59a3a2c688056b8771f2b536a04a92
Reece Martin <capt.rm.fae@gmail.com>
2018-05-31 14:50:24 -0700

    Converted file to Kotlin

--- changedetection/ClassDescription.kt
+++ changedetection/ClassDescription.kt
@@ -1,1 +1,24 @@
+// TODO add javadoc, and unittest
 
+import java.util.ArrayList
+
+class ClassDescription(functionName: String, numArgs: Int, args: ArrayList<FunctionDescription>, modifier: String) {
+
+    var accessModifier: String? = null
+        get() = retType
+    var className: String? = null
+    var numFunctions: Int = 0
+        private set
+    var functions: ArrayList<FunctionDescription>? = null
+
+    fun setNumArgs(numFunctions: Int) {
+        this.numFunctions = numFunctions
+    }
+
+    init {
+        this.className = className
+        this.numFunctions = numFunctions
+        this.functions = functions
+        this.accessMdifier = modifier
+    }
+}
