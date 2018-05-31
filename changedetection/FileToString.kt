56c5c614084eb7511902cabef1735cc4cbde4b3f
Reece Martin <capt.rm.fae@gmail.com>
2018-05-31 14:48:43 -0700

    Converted to Kotlin File

--- changedetection/FileToString.kt
+++ changedetection/FileToString.kt
@@ -1,1 +1,39 @@
+// TODO Add Unittest
 
+import java.io.*
+
+object FileToString {
+    /**
+     * Returns a String containing all the characters from the file at filePath, with all the newlines and spaces
+     * removed.
+     *
+     * @param filePath : String
+     * @return String/null
+     */
+    fun convertToString(filePath: String): String? {
+        val file = File(filePath)
+        val s = StringBuilder()
+        try {
+            val `in` = BufferedReader(FileReader(file))
+            var buf: String? = `in`.readLine()
+            while (buf != null) {
+                if (buf.trim { it <= ' ' } != "") {
+                    s.append(buf.trim { it <= ' ' })
+                    s.append(" ")
+                }
+                buf = `in`.readLine()
+            }
+            return s.toString()
+        } catch (iox: IOException) {
+            iox.printStackTrace()
+        }
+
+        return null
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val filePath = "/* Bring up Dialog Potentially to get file or use cwd */"
+        val res = convertToString(filePath)
+    }
+}
