b0d7ef0e9dbd1b2e9db1aeb18a451b6e9e2cc021
Reece Martin <capt.rm.fae@gmail.com>
2018-05-31 14:46:56 -0700

    Converted this file to Kotlin

--- changedetection/FunctionDescription.kt
+++ changedetection/FunctionDescription.kt
@@ -1,1 +1,14 @@
+// TODO add javadoc, and unittest
 
+import java.util.*
+
+class FunctionDescription
+// Access Modifier
+
+// May need to expand definition to include access rights etc.
+(var functionName: String?, var numArgs: Int, var args: ArrayList<String>?,
+        // Getter
+
+        // Setter
+
+ var retType: String?/* Added access modifier*/)// Access Modifier
