333933af3f2fd0e4a4b0395382d287a51b1d3d19
Reece Martin <capt.rm.fae@gmail.com>
2018-06-09 13:40:26 -0700

    Making Detector Interface and DetectorImpl to implement it and store common methods

--- changedetection/JavaDetector.java
+++ changedetection/JavaDetector.java
@@ -8,33 +8,9 @@ Notes: Have someone write code to give only the code within the first set of cur
 it will then be very unlikely to have duplicates and reserved words.
  */
 
-public class JavaDetector {
+public class JavaDetector extends DetectorImpl {
 
   public List<String> reserved = new ArrayList();
-
-  /**
-   * Returns a substring of the second string from the point where the strings differ until the end
-   * of the second string
-   *
-   * @param str1 the first string
-   * @param str2 the second string to be compared with the first
-   * @return String
-   */
-  public static String difference(String str1, String str2) {
-    if (str1 == null && str2 == null) {
-      return "";
-    }
-
-    // if str1 is null indexOfDifference will return 0 and str2 will be returned
-    if (str2 == null) {
-      return str1;
-    }
-    int changeIndex = indexOfDifference(str1, str2);
-    if (changeIndex == -1) {
-      return "";
-    }
-    return str2.substring(changeIndex);
-  }
 
   /**
    * TODO Finish this function
@@ -43,29 +19,6 @@ public class JavaDetector {
    */
   public static List<String> getReservedLibraries(List<String> libraries) {
     return null;
-  }
-
-  /**
-   * Compares two strings and returns the index of the first difference between the strings
-   *
-   * @param str1 the first string
-   * @param str2 the second string to be compared with the first
-   * @return String
-   */
-  public static int indexOfDifference(String str1, String str2) {
-    if (str1.equals(str2)) {
-      return -1;
-    }
-    if (str1 == null || str2 == null) {
-      return 0;
-    }
-    int i;
-    for (i = 0; i < str1.length() && i < str2.length(); i++) {
-      if (str1.charAt(i) != str2.charAt(i)) {
-        return i;
-      }
-    }
-    return -1;
   }
 
   /**
