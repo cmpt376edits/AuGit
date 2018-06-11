dbc0693ba449260395e013bcc7542a62d832657d
Reece Martin <capt.rm.fae@gmail.com>
2018-06-10 21:55:32 -0700

    Added Test Shells

--- changedetection/JavaDetectorImplTest.java
+++ changedetection/JavaDetectorImplTest.java
@@ -1,3 +1,21 @@
 public class JavaDetectorImplTest {
-    // TODO (rmartin) WRITE A TEST WITH ASSERTJ
+  @Test
+  public void detectFunctionsTest(String origin, String current) {
+    // Test Code Here
+  }
+
+  @Test  
+  public void detectClassesTest(String origin, String current){
+    // Test Code Here
+  }
+
+  @Test  
+  public void getMessageFunctionsJavaTest(ArrayList<FunctionDescription> functionDescriptions) {
+    // Test Code Here
+  }
+    
+  @Test
+  public void getMessageClassesJavaTest(ArrayList<ClassDescription> classDescriptions) {
+    // Test Code Here
+  }
 }
