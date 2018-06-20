26791f128d48ab3f8ce2bf24de13f4b07d6f9c36
Reece Martin <capt.rm.fae@gmail.com>
2018-06-20 08:39:44 -0700

    Added initial message function

--- changedetection/FunctionDescription.java
+++ changedetection/FunctionDescription.java
@@ -19,10 +19,31 @@ public class FunctionDescription {
     this.accessMod = accessMod;
   }
 
-  public String toMessage(){
-    return "";
-    // This needs to have args to change the message
-  }
+  public String toMessage() {
+    StringBuilder message = new StringBuilder();
+      message.append("Created function ");
+      if (functionDescription.getRetType().equals("void")) {
+        message.append("with void return type, called ");
+      } else {
+        message.append("with ");
+        message.append(functionDescription.getRetType());
+        message.append(" return type, called ");
+      }
+      message.append(functionDescription.getFunctionName());
+      message.append(" and arguments as follows: ");
+      for (int j = 0; j < functionDescription.getArgs().size(); j++) {
+        message.append(functionDescription.getArgs().get(j));
+        if (j + 1 == functionDescription.getArgs().size()) {
+
+        } else {
+          message.append(", ");
+        }
+      }
+      message.append(".");
+    }
+
+    return message.toString();
+}
 
   /*
 
