6f467a59fa8c6100487c767e01bc098b51f5adff
Reece Martin <capt.rm.fae@gmail.com>
2018-06-22 09:19:50 -0700

    Update JavaDetectorImpl.java

--- changedetection/JavaDetectorImpl.java
+++ changedetection/JavaDetectorImpl.java
@@ -257,27 +257,9 @@ public class JavaDetectorImpl extends DetectorImpl {
     StringBuilder message = new StringBuilder();
 
     for (ClassDescription classDescription : classDescriptions) {
-      message.append("Created function ");
-      //      if (Description.getRetType().equals("void")) {
-      //        message.append("with void return type, called ");
-      //      } else {
-      //        message.append("with ");
-      //        message.append(functionDescription.getRetType());
-      //        message.append(" return type, called ");
-      //      }
-      //      message.append(functionDescription.getFunctionName());
-      //      message.append(" and arguments as follows: ");
-      //      for (int j = 0; j < functionDescription.getArgs().size(); j++) {
-      //        message.append(functionDescription.getArgs().get(j));
-      //        if (j + 1 == functionDescription.getArgs().size()) {
-      //
-      //        } else {
-      //          message.append(", ");
-      //        }
-      //      }
-      //      message.append(".");
-      //    }
+      message.append(classDescription.getMessage());
     }
+
     return message.toString();
   }
 }
