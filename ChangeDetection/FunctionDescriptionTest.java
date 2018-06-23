7ed4d42b91ffcc1ff6cfab264c98443d1d98e25a
Reece Martin <capt.rm.fae@gmail.com>
2018-06-23 16:37:38 -0700

    Adding tests in FunctionDescriptionTest

--- ChangeDetection/FunctionDescriptionTest.java
+++ ChangeDetection/FunctionDescriptionTest.java
@@ -15,21 +15,39 @@ public class FunctionDescriptionTest {
     this.func = new FunctionDescription("genericFunction", 3, args, "void", "public", false);
   }
 
+  public void setUpDel() {
+    List<String> args = new ArrayList<>();
+    args.add("a");
+    args.add("b");
+    args.add("c");
+    this.func = new FunctionDescription("genericFunction", 3, args, "void", "public", true);
+  }
+
   @Test
   public void toMessageTestNoArgs() {
     setUp();
     String message = this.func.toMessage();
     assert (message)
-            .equals(
-                    "Created function with void return type, called genericFunction and arguments as follows: a, b, c.");
+        .equals(
+            "Created function with void return type, called genericFunction and arguments as follows: a, b, c.");
   }
 
   @Test
   public void toMessageTestDeletedNoArgs() {
-    setUp();
+    setUpDel();
     String message = this.func.toMessage();
     assert (message)
-            .equals(
-                    "Deleted function with void return type, called genericFunction and arguments as follows: a, b, c.");
+        .equals(
+            "Deleted function with void return type, called genericFunction and arguments as follows: a, b, c.");
+  }
+
+  @Test
+  public void toMessageTestZeroArg() {
+    setUp();
+    String message = this.func.toMessage(0);
+    System.out.println(message);
+    assert (message)
+        .equals(
+            "Deleted function with void return type, called genericFunction and arguments as follows: a, b, c.");
   }
 }
