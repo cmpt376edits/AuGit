f68fe7e6b3d96277214fe59b0f46e4fe526ffbdf
Reece Martin <capt.rm.fae@gmail.com>
2018-06-23 14:28:27 -0700

    Reformatting

--- changedetection/FunctionDescription.java
+++ changedetection/FunctionDescription.java
@@ -21,27 +21,27 @@ public class FunctionDescription {
 
   public String toMessage() { // Todo Args
     StringBuilder message = new StringBuilder();
-      message.append("Created function ");
-      if (this.getRetType().equals("void")) {
-        message.append("with void return type, called ");
-      } else {
-        message.append("with ");
-        message.append(this.getRetType());
-        message.append(" return type, called ");
-      }
-      message.append(this.getFunctionName());
-      message.append(" and arguments as follows: ");
-      for (int j = 0; j < this.getArgs().size(); j++) {
-        message.append(this.getArgs().get(j));
-        if (j + 1 == this.getArgs().size()) {
-
-        } else {
-          message.append(", ");
-        }
-      }
-      message.append(".");
-      return message.toString();
+    message.append("Created function ");
+    if (this.getRetType().equals("void")) {
+      message.append("with void return type, called ");
+    } else {
+      message.append("with ");
+      message.append(this.getRetType());
+      message.append(" return type, called ");
     }
+    message.append(this.getFunctionName());
+    message.append(" and arguments as follows: ");
+    for (int j = 0; j < this.getArgs().size(); j++) {
+      message.append(this.getArgs().get(j));
+      if (j + 1 == this.getArgs().size()) {
+
+      } else {
+        message.append(", ");
+      }
+    }
+    message.append(".");
+    return message.toString();
+  }
 
   /*
 
