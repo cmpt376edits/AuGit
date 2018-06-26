c0ac5d5f7e8a4d7953fda37627865960231366e0
Reece Martin <capt.rm.fae@gmail.com>
2018-06-26 08:42:22 -0700

    Test Conversion

--- tests/ChangeDetectionTests/ClassDescriptionTest.java
+++ tests/ChangeDetectionTests/ClassDescriptionTest.java
@@ -1,6 +1,6 @@
 package ChangeDetectionTests;
 
-import changedetection.FunctionDescription;
+import changedetection.ClassDescription;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
