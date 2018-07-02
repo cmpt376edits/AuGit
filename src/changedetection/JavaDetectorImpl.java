49b02f980d7c1300e86fa072cf5d251a3870f194
Reece Martin <capt.rm.fae@gmail.com>
2018-07-02 15:27:23 -0700

    Adding outline for Java Function Detector Function

--- src/changedetection/JavaDetectorImpl.java
+++ src/changedetection/JavaDetectorImpl.java
@@ -56,7 +56,11 @@ public class JavaDetectorImpl extends DetectorImpl {
    */
   public void detectFunctions(String code, String old) {
     // Get the two strings from both git and using the projutil.converttostring function
+
+    
     // Get the difference of those strings with a new function
+
+    // Use a string find util with regex to find any function defs
   }
 
   /**
