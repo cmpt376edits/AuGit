6bda86b14445796b37658d506241095c8d5c58f6
Reece Martin <capt.rm.fae@gmail.com>
2018-07-03 08:11:14 -0700

    Update Detector.java

--- src/changedetection/Detector.java
+++ src/changedetection/Detector.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public interface Detector {
 
+  // This function takes the origin (last commited code), and current code and finds the newly defined or deleted
+  // functions between the two and sets the detectors function list to a list of function description objects.
   void detectFunctions(String origin, String current);
 
   void detectClasses(String origin, String current);
