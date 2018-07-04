d9cb877efebf0ecf1f7c361784bbb89c02210260
Reece Martin <capt.rm.fae@gmail.com>
2018-07-04 08:05:34 -0700

    Adding util function

--- src/utilities/ProjUtil.java
+++ src/utilities/ProjUtil.java
@@ -76,6 +76,30 @@ public class ProjUtil {
     }
     return -1;
   }
+  
+  /**
+   * Returns a substring of the second string from the point where the strings differ until the end
+   * of the second string
+   *
+   * @param str1 the first string
+   * @param str2 the second string to be compared with the first
+   * @return String
+   */
+  public static String difference(String str1, String str2) {
+    if (str1 == null && str2 == null) {
+      return "";
+    }
+
+    // if str1 is null indexOfDifference will return 0 and str2 will be returned
+    if (str2 == null) {
+      return str1;
+    }
+    int changeIndex = indexOfDifference(str1, str2);
+    if (changeIndex == -1) {
+      return "";
+    }
+    return str2.substring(changeIndex);
+  }
 
   public static void main(String[] args) {
     // String filePath = "/* Bring up Dialog Potentially to get file or use cwd */";
