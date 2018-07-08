131530964a92ed9020d20e9e18a598462cd0d986
Reece Martin <capt.rm.fae@gmail.com>
2018-07-08 14:51:45 -0700

    Formatting . . .

--- src/changedetection/Detector.java
+++ src/changedetection/Detector.java
@@ -6,15 +6,15 @@ import java.util.ArrayList;
 
 public interface Detector {
 
-  // This function takes the origin (last commited code), and current code and finds the newly defined or deleted
-  // functions between the two and sets the detectors function list to a list of function description objects.
-  void detectFunctions(String origin, String current);
+    // This function takes the origin (last commited code), and current code and finds the newly defined or deleted
+    // functions between the two and sets the detectors function list to a list of function description objects.
+    void detectFunctions(String origin, String current);
 
-  // This function takes the origin (last commited code), and current code and finds the newly defined or deleted
-  // classes between the two and sets the detectors class list to a list of class description objects.
-  void detectClasses(String origin, String current);
+    // This function takes the origin (last commited code), and current code and finds the newly defined or deleted
+    // classes between the two and sets the detectors class list to a list of class description objects.
+    void detectClasses(String origin, String current);
 
-  String getMessageFunctions();
+    String getMessageFunctions();
 
-  String getMessageClasses();
+    String getMessageClasses();
 }
