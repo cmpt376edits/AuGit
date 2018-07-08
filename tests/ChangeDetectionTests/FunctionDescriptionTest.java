b96880330300b501ffdce5d82babdab30663f4c3
Reece Martin <capt.rm.fae@gmail.com>
2018-07-08 15:04:17 -0700

    Formatting . . .

--- tests/ChangeDetectionTests/FunctionDescriptionTest.java
+++ tests/ChangeDetectionTests/FunctionDescriptionTest.java
@@ -8,87 +8,87 @@ import java.util.List;
 
 class FunctionDescriptionTest {
 
-  private FunctionDescription func;
+    private FunctionDescription func;
 
-  private void setUp() {
-    List<String> args = new ArrayList<>();
-    args.add("a");
-    args.add("b");
-    args.add("c");
-    this.func = new FunctionDescription("genericFunction", 3, args, "void", "public", false);
-  }
+    private void setUp() {
+        List<String> args = new ArrayList<>();
+        args.add("a");
+        args.add("b");
+        args.add("c");
+        this.func = new FunctionDescription("genericFunction", 3, args, "void", "public", false);
+    }
 
-  private void setUpDel() {
-    List<String> args = new ArrayList<>();
-    args.add("a");
-    args.add("b");
-    args.add("c");
-    this.func = new FunctionDescription("genericFunction", 3, args, "void", "public", true);
-  }
+    private void setUpDel() {
+        List<String> args = new ArrayList<>();
+        args.add("a");
+        args.add("b");
+        args.add("c");
+        this.func = new FunctionDescription("genericFunction", 3, args, "void", "public", true);
+    }
 
-  @org.junit.jupiter.api.Test
-  void toMessageTestNoArgs() {
-    setUp();
-    String message = this.func.toMessage();
-    assert (message)
-        .equals(
-            "Created function with void return type, called genericFunction and arguments as follows: a, b, c.");
-  }
+    @org.junit.jupiter.api.Test
+    void toMessageTestNoArgs() {
+        setUp();
+        String message = this.func.toMessage();
+        assert (message)
+                .equals(
+                        "Created function with void return type, called genericFunction and arguments as follows: a, b, c.");
+    }
 
-  @Test
-  void toMessageTestDeletedNoArgs() {
-    setUpDel();
-    String message = this.func.toMessage();
-    assert (message)
-        .equals(
-            "Deleted function with void return type, called genericFunction and arguments as follows: a, b, c.");
-  }
+    @Test
+    void toMessageTestDeletedNoArgs() {
+        setUpDel();
+        String message = this.func.toMessage();
+        assert (message)
+                .equals(
+                        "Deleted function with void return type, called genericFunction and arguments as follows: a, b, c.");
+    }
 
-  @Test
-  void toMessageTestZeroArg() {
-    setUp();
-    String message = this.func.toMessage(0);
-    assert (message).equals("Created public function genericFunction.");
-  }
+    @Test
+    void toMessageTestZeroArg() {
+        setUp();
+        String message = this.func.toMessage(0);
+        assert (message).equals("Created public function genericFunction.");
+    }
 
-  @Test
-  void toMessageTestOneArg() {
-    setUp();
-    String message = this.func.toMessage(1);
-    assert (message)
-        .equals("Created public function genericFunction with the following argument(s) a b c .");
-  }
+    @Test
+    void toMessageTestOneArg() {
+        setUp();
+        String message = this.func.toMessage(1);
+        assert (message)
+                .equals("Created public function genericFunction with the following argument(s) a b c .");
+    }
 
-  @Test
-  void toMessageTestTwoArg() {
-    setUp();
-    String message = this.func.toMessage(2);
-    assert (message)
-        .equals(
-            "Created public function genericFunction with the following argument(s) a b c and void return type.");
-  }
+    @Test
+    void toMessageTestTwoArg() {
+        setUp();
+        String message = this.func.toMessage(2);
+        assert (message)
+                .equals(
+                        "Created public function genericFunction with the following argument(s) a b c and void return type.");
+    }
 
-  @Test
-  void toMessageTestDeletedZeroArg() {
-    setUpDel();
-    String message = this.func.toMessage(0);
-    assert (message).equals("Deleted public function genericFunction.");
-  }
+    @Test
+    void toMessageTestDeletedZeroArg() {
+        setUpDel();
+        String message = this.func.toMessage(0);
+        assert (message).equals("Deleted public function genericFunction.");
+    }
 
-  @Test
-  void toMessageTestDeletedOneArg() {
-    setUpDel();
-    String message = this.func.toMessage(1);
-    assert (message)
-        .equals("Deleted public function genericFunction with the following argument(s) a b c .");
-  }
+    @Test
+    void toMessageTestDeletedOneArg() {
+        setUpDel();
+        String message = this.func.toMessage(1);
+        assert (message)
+                .equals("Deleted public function genericFunction with the following argument(s) a b c .");
+    }
 
-  @Test
-  void toMessageTestDeletedTwoArg() {
-    setUpDel();
-    String message = this.func.toMessage(2);
-    assert (message)
-        .equals(
-            "Deleted public function genericFunction with the following argument(s) a b c and void return type.");
-  }
+    @Test
+    void toMessageTestDeletedTwoArg() {
+        setUpDel();
+        String message = this.func.toMessage(2);
+        assert (message)
+                .equals(
+                        "Deleted public function genericFunction with the following argument(s) a b c and void return type.");
+    }
 }
