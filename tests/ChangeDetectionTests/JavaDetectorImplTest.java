a94e1cd8cac15afa265034c19f9828b9801fabbd
Reece Martin <capt.rm.fae@gmail.com>
2018-07-08 15:04:59 -0700

    Formatting . . .

--- tests/ChangeDetectionTests/JavaDetectorImplTest.java
+++ tests/ChangeDetectionTests/JavaDetectorImplTest.java
@@ -4,32 +4,33 @@ import org.junit.Ignore;
 
 public class JavaDetectorImplTest {
 
-  private static final String SAMPLE_CODE_ORIGIN = "";
+    private static final String SAMPLE_CODE_ORIGIN = "";
 
-  private static final String SAMPLE_CODE_CURRENT =
-      "  public boolean equals(Object o) {\n"
-          + "    if (this == o) return true;\n"
-          + "    if (o == null || getClass() != o.getClass()) return false;\n"
-          + "    JavaDetectorImpl that = (JavaDetectorImpl) o;\n"
-          + "    return Objects.equals(reserved, that.reserved)\n"
-          + "        && Objects.equals(funcDescList, that.funcDescList)\n"
-          + "        && Objects.equals(classDescList, that.classDescList);\n"
-          + "  }\n";
+    private static final String SAMPLE_CODE_CURRENT =
+            "  public boolean equals(Object o) {\n"
+                    + "    if (this == o) return true;\n"
+                    + "    if (o == null || getClass() != o.getClass()) return false;\n"
+                    + "    JavaDetectorImpl that = (JavaDetectorImpl) o;\n"
+                    + "    return Objects.equals(reserved, that.reserved)\n"
+                    + "        && Objects.equals(funcDescList, that.funcDescList)\n"
+                    + "        && Objects.equals(classDescList, that.classDescList);\n"
+                    + "  }\n";
 
-  @org.junit.Test
-  public void detectFunctionsTest() {
-  }
+    @org.junit.Test
+    public void detectFunctionsTest() {
+    }
 
-  @Ignore
-  public void detectClassesTest() {}
+    @Ignore
+    public void detectClassesTest() {
+    }
 
-  @Ignore
-  public void getMessageFunctionsJavaTest() {
-    // Test Code Here
-  }
+    @Ignore
+    public void getMessageFunctionsJavaTest() {
+        // Test Code Here
+    }
 
-  @Ignore
-  public void getMessageClassesJavaTest() {
-    // Test Code Here
-  }
+    @Ignore
+    public void getMessageClassesJavaTest() {
+        // Test Code Here
+    }
 }
