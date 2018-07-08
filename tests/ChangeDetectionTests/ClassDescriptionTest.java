1e8b9722d9ee238773ea13414b8502a12a73d646
Reece Martin <capt.rm.fae@gmail.com>
2018-07-07 19:58:35 -0700

    Update ClassDescriptionTest.java

--- tests/ChangeDetectionTests/ClassDescriptionTest.java
+++ tests/ChangeDetectionTests/ClassDescriptionTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClassDescriptionTest {
+  // TODO copy some tests from functionDescription test
 
   private ClassDescription clas;
 
